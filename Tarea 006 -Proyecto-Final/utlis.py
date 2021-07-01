8bbb43d5321616f64dc8ad929954b1ba7ada00d6
Yerson sanchez <37232721+yerson001@users.noreply.github.com>
2021-07-01 08:30:41 -0500

    Add files via upload

--- Tarea 006 -Proyecto-Final/utlis.py
+++ Tarea 006 -Proyecto-Final/utlis.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-## TO STACK ALL THE IMAGES IN ONE WINDOW
+
 def stackImages(imgArray,scale,lables=[]):
     rows = len(imgArray)
     cols = len(imgArray[0])
@@ -31,7 +31,6 @@ def stackImages(imgArray,scale,lables=[]):
     if len(lables) != 0:
         eachImgWidth= int(ver.shape[1] / cols)
         eachImgHeight = int(ver.shape[0] / rows)
-        #print(eachImgHeight)
         for d in range(0, rows):
             for c in range (0,cols):
                 cv2.rectangle(ver,(c*eachImgWidth,eachImgHeight*d),(c*eachImgWidth+len(lables[d][c])*13+27,30+eachImgHeight*d),(255,255,255),cv2.FILLED)
@@ -65,6 +64,7 @@ def biggestContour(contours):
                 biggest = approx
                 max_area = area
     return biggest,max_area
+
 def drawRectangle(img,biggest,thickness):
     cv2.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 255, 0), thickness)
     cv2.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 255, 0), thickness)
